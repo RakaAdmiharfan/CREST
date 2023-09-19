128af28bc75209a81cb74cb8bcbb0f1f4553725b
amjadadhie <prasetyoamjad@gmail.com>
2023-09-19 16:40:34 +0700

    image

--- src/app/simulasi/page.tsx
+++ src/app/simulasi/page.tsx
@@ -5,10 +5,7 @@ import "./page.css";
 export default function Home() {
   return (
     <main>
-      <div className="bg-[url('../assets/gedung.svg')] w-full bg-cover origin-center h-[480px] flex flex-col justify-center items-center">
-        <h1>Invest Easier</h1>
-        <h5>Pick our services suited for your needs</h5>
-      </div>
+      
       <div className="bg-slate-100 flex justify-center items-center gap-52 px-52 py-8">
         <div className="flex flex-col justify-center items-center text-center gap-8">
           <h3>Marketplace</h3>
